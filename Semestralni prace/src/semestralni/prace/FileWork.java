5218e7832bf27e5339f75c660fe42bca9faebb13
David <David@192.168.0.102>
2013-03-14 12:47:34 +0100

    vypis

---
+++ Semestralni prace/src/semestralni/prace/FileWork.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semestralni.prace;
+
+/**
+ *
+ * @author David
+ */
+public class FileWork {
+    public void Save(){
+        
+    }
+    public 
+    
+}
