504ade8f1e68aa1154b7fb5aff336969e1047243
David <David@192.168.0.102>
2013-03-16 00:57:54 +0100

    vypis

--- Semestralni prace/src/semestralni/prace/Surovina.java
+++ Semestralni prace/src/semestralni/prace/Surovina.java
@@ -4,49 +4,107 @@
  */
 package semestralni.prace;
 
+import java.util.Objects;
+
 /**
  *
  * @author David
  */
-public class Surovina extends AbstractSurovina {
+public class Surovina implements InterfaceSurovina {
+      private String nazev ;
+      private double spotreba ;
+      private double mnozstvi ;
+      private double cena     ;
+
+    @Override
+    public String toString() {
+        return "Surovina{" + "nazev=" + this.nazev + ", spotreba=" + spotreba + ", mnozstvi=" + mnozstvi + ", cena=" + cena + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 43 * hash + Objects.hashCode(this.nazev);
+        hash = 43 * hash + (int) (Double.doubleToLongBits(this.spotreba) ^ (Double.doubleToLongBits(this.spotreba) >>> 32));
+        hash = 43 * hash + (int) (Double.doubleToLongBits(this.mnozstvi) ^ (Double.doubleToLongBits(this.mnozstvi) >>> 32));
+        hash = 43 * hash + (int) (Double.doubleToLongBits(this.cena) ^ (Double.doubleToLongBits(this.cena) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Surovina other = (Surovina) obj;
+        if (!Objects.equals(this.nazev, other.nazev)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.spotreba) != Double.doubleToLongBits(other.spotreba)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.mnozstvi) != Double.doubleToLongBits(other.mnozstvi)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.cena) != Double.doubleToLongBits(other.cena)) {
+            return false;
+        }
+        return true;
+    }
+     
     
+    public Surovina(String nazev,double spotreba,double mnozstvi,double cena) {
+         this.nazev = nazev;
+         this.spotreba = spotreba;
+         this.mnozstvi = mnozstvi;
+         this.cena = cena;
+    }
+
     @Override
-    String getNazev() {
+    public String getNazev() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
     @Override
-    void setNazev(String nazev) {
+    public void setNazev(String nazev) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
     @Override
-    double getSpotreba() {
+    public double getSpotreba() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
     @Override
-    void setSpotreba(double spotreba) {
+    public void setSpotreba(double spotreba) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
     @Override
-    double getMnozstvi() {
+    public double getMnozstvi() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
     @Override
-    void setMnozstvi(double mnozstvi) {
+    public void setMnozstvi(double mnozstvi) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
     @Override
-    double getCena() {
+    public double getCena() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
     @Override
-    void setCena(double cena) {
+    public void setCena(double cena) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    public Surovina(String nazev,double cena,double mnozstvi,double spotreba){
-        nazev    = super.   nazev;
-        cena     = super.    cena;
-        mnozstvi = super.mnozstvi;
-        spotreba = super.spotreba;
-        
-    }
+
+  
+     
+ 
+    
 }
