70fe6ba1e89c0dc4ab404f160898bdad35f3cbb0
David <David@192.168.0.102>
2013-03-15 01:00:46 +0100

    vypis

--- Semestralni prace/src/semestralni/prace/Surovina.java
+++ Semestralni prace/src/semestralni/prace/Surovina.java
@@ -8,6 +8,45 @@ package semestralni.prace;
  *
  * @author David
  */
-public class Surovina {
+public class Surovina extends AbstractSurovina {
     
+    @Override
+    String getNazev() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    @Override
+    void setNazev(String nazev) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    @Override
+    double getSpotreba() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    @Override
+    void setSpotreba(double spotreba) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    @Override
+    double getMnozstvi() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    @Override
+    void setMnozstvi(double mnozstvi) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    @Override
+    double getCena() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    @Override
+    void setCena(double cena) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public Surovina(String nazev,double cena,double mnozstvi,double spotreba){
+        nazev    = super.   nazev;
+        cena     = super.    cena;
+        mnozstvi = super.mnozstvi;
+        spotreba = super.spotreba;
+        
+    }
 }
