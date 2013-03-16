45cfba11ef3d1a3ec6b36681cdaf0396a9826c0e
David <David@192.168.0.102>
2013-03-16 18:33:57 +0100

    vypis

---
+++ Semestralni prace/src/semestralni/prace/InterfaceSurovina.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semestralni.prace;
+
+/**
+ *
+ * @author David
+ */
+public interface InterfaceSurovina {
+    
+    public String getNazev();
+    public void   setNazev(String nazev); 
+    public double getSpotreba(); 
+    public void   setSpotreba(double spotreba); 
+    public double getMnozstvi(); 
+    public void   setMnozstvi(double mnozstvi); 
+    public double getCena(); 
+    public void   setCena(double cena);     
+}
