a2e44b5f2868a4fed96ba5c4e6d82176144e5749
David <David@192.168.0.102>
2013-03-16 19:20:02 +0100

    vypis

--- Semestralni prace/src/semestralni/prace/SemestralniPrace.java
+++ Semestralni prace/src/semestralni/prace/SemestralniPrace.java
@@ -15,12 +15,16 @@ public class SemestralniPrace {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Surovina s = new Surovina("a",1,2,3);       
-        System.out.println(s.toString());
-        s.setNazev("B");
-        String a = s.getNazev();
-        System.out.println(a);
-        System.out.println(s.toString());
+        Surovina s1 = new Surovina("a",1,2,3);       
+         Surovina s2 = new Surovina("b",4,5,6);   
+          Surovina s3 = new Surovina("c",7,8,9);   
+          SeznamSurovin seznam = new SeznamSurovin();
+          seznam.pridejSurovinu(s1);
+          seznam.pridejSurovinu(s2);
+          seznam.pridejSurovinu(s3);
+         seznam.vypisSuroviny();
+        
+        
         
         
     }
